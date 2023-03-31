75b6ddcd6b5c0a2240c252455fda648d1c0c4327
ironny04 <63394735+ironny04@users.noreply.github.com>
2023-03-31 20:31:12 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,13 +5,7 @@ const OPENAI_API_HOST = "api.openai.com";
 serve(async (request) => {
   const url = new URL(request.url);
 
-  if (url.pathname === "/") {
-    return new Response(await Deno.readTextFile("./Readme.md"), {
-      headers: {
-        "content-type": "text/plain;charset=UTF-8",
-      },
-    });
-  }
+
 
   url.host = OPENAI_API_HOST;
   return await fetch(url, request);
